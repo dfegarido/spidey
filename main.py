90dfbbd0d959f7ab01acb412eca205a959f643aa
rorounifix <rorounifix@gmail.com>
2017-02-23 17:11:01 +0300

    Update

--- main.py
+++ main.py
@@ -5,20 +5,60 @@ from bs4 import BeautifulSoup
 import requests
 
 #url = raw_input("Url : ")
-url = "azalflooring.com"
+url = "azal.com/main.html"
 
-# create a request 
-r = requests.get("http://"+url)
+def crawl(url):
+    
+    r = requests.get("http://"+url)
+    data = r.text
+    
 
-# get the source page from html
-data = r.text
+    if(name == "nt"):
+        soup = BeautifulSoup(data, "html.parser")
+        li_parser = link(soup)
+        #print(li_parser)
+        get_item_link(li_parser)
+        
+    else:
+        soup = BeautifulSoup(data, "lxml")
+        link(soup)
+        
+        
 
-# check the Os System if windows or Linux
-if(name == "nt"):
-    soup = BeautifulSoup(data, "html.parser")
-else:
-    soup = BeautifulSoup(data, "lxml")
+ 
 
-# take all a link from this site
-for link in soup.find_all("a"):
-    print(link.get("href"))
+def link(soup):
+    page = 0
+    y = []
+    for l in soup.find_all("a"):
+        li = l.get("href")
+        #fp = open(url+".txt","a")
+        #fp.write(li+"\n")
+        #fp.close()
+        y.append(li)
+        #print(y)
+        page += 1
+    return y
+    
+    
+
+   
+            
+
+def get_item_link(x):
+    page = 0
+    try:
+        for i in x:
+            r = requests.get(i)
+            data = r.text
+            soup = BeautifulSoup(data, "html.parser")
+            print(soup)
+            page += 1
+    except KeyboardInterrupt:
+        print("Exit !!!")
+        
+
+
+if __name__=="__main__":   
+    crawl(url)
+    #get_item_link()
